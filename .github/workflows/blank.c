7fa311390e58a19219a19accd23a43bccbae048f
Ousman abakar <47245197+ousmanabakar@users.noreply.github.com>
2020-03-17 20:09:03 +0300

    c

---
+++ .github/workflows/blank.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    char isim[30];
+    int toplammaas,toplamsaat,saatlikucret;
+    float ekucret;
+
+
+    printf("calisanin ismi:");
+    scanf("%s",isim);
+
+    printf("Toplam-saati:");
+    scanf("%d",&toplamsaat);
+
+    printf("Saatlik  ucret:");
+    scanf("%d",&saatlikucret);
+
+    toplammaas = toplamsaat*saatlikucret;
+    printf("Toplam maas:%d\n",toplammaas);
+
+    if(toplamsaat>40)
+    {
+
+        toplammaas = toplammaas+saatlikucret;
+        printf("Toplam maas:");
+        scanf("%d",&toplammaas);
+
+        printf("Saatlik ucret:");
+        scanf("%d",&saatlikucret);
+
+        ekucret=0.5*(toplamsaat-40)*saatlikucret;
+        printf("Ek Ucret:%.2f",ekucret);
+
+    }
+    else{
+		printf	
+
+    return 0;
+}
